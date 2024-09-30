6d7b19c3efd3baa5678be6249b80cc6a9edf26e6
Joshy3282 <joshkanalas1119@gmail.com>
2024-09-30 14:09:16 -0500

    Revert "change bedrock data path and fix radioactive interface"
    This reverts commit 3a967348a790ca9b29983af17568fe919f5233d6.

--- src/block/utils/Radioactive.php
+++ src/block/utils/Radioactive.php
@@ -2,13 +2,11 @@
 
 namespace pocketmine\block\utils;
 
-use pocketmine\math\AxisAlignedBB;
-
 interface Radioactive
 {
 	public function getRadioactivityRadius(): float;
 
 	public function getRadioactivityStrength(): float;
 
-	public function tickRadioactivity(AxisAlignedBB $bb): bool;
+	public function tickRadioactivity() : bool;
 }
