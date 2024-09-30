6ce6a70f774a30185072f153c22df7a1c795b36a
Joshy3282 <joshkanalas1119@gmail.com>
2024-09-30 14:09:22 -0500

    Revert "add radioactive block addition, and damage"
    This reverts commit 5a848147d9dde8200c8504650f86a78ab1a6f502.

--- src/event/entity/EntityDamageEvent.php
+++ src/event/entity/EntityDamageEvent.php
@@ -65,7 +65,6 @@ class EntityDamageEvent extends EntityEvent implements Cancellable{
 	public const CAUSE_CUSTOM = 14;
 	public const CAUSE_STARVATION = 15;
 	public const CAUSE_FALLING_BLOCK = 16;
-	public const CAUSE_RADIOACTIVITY = 17;
 
 	private float $baseDamage;
 	private float $originalBase;
